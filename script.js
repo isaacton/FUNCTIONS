ec1f4b3026048611a9c6dd3e17923e25cbdd48fa
ISAAC ANINTA NDOH <45712236+isaacton@users.noreply.github.com>
2022-07-22 13:54:05 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// function that takes parametars
+function add(num1,num2){
+  
+  let result = num1 + num2;
+  
+  // alerting result in browser
+  alert(result);
+  
+}
+
+// calling the function
+alert(add(500,200));
